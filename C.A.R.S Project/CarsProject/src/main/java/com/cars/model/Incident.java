08ade63fa60c7fde576477f0a292686f99a7c722
Purnendu Shekhar Achary <98398780+purnenduachary@users.noreply.github.com>
2025-05-04 00:44:43 +0530

    Add files via upload

---
+++ C.A.R.S Project/CarsProject/src/main/java/com/cars/model/Incident.java
@@ -0,0 +1,22 @@
+package com.cars.model;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class Incident {
+	
+	private int IncidentId;
+	private String IncidentType;
+	private Date IncidentDate;
+	private double Latitude;
+	private double Longitude;
+	private String Description;
+	private String Status;
+	private int VictimId;
+	private int SuspectId;
+	private int officerId;
+
+}
